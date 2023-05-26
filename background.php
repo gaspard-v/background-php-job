bc1b8fc9d9897257ca3713dd5a788ef41a3cf2a3
gaspard-v <60385905+gaspard-v@users.noreply.github.com>
2023-05-26 10:55:45 +0200

    add function_exists for fastcgi

--- background.php
+++ background.php
@@ -7,7 +7,7 @@ header('Content-Length: '.ob_get_length());
 http_response_code(200);
 ob_end_flush();
 flush();
-if (strpos($_SERVER['SERVER_SOFTWARE'], 'FPM')) {
+if (function_exists("fastcgi_finish_request")) {
     fastcgi_finish_request();
 }
 set_time_limit(0);
